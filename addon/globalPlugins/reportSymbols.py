e98085f4eaced092748d2c22d9815a8d2f63ca6e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2016-07-26 22:43:14 +0200

    Initial commit.

---
+++ addon/globalPlugins/reportSymbols.py
@@ -0,0 +1,69 @@
+﻿# reportSymbols: Plugin to listen the typed symbols (non alphanumeric characters)
+# Date: 26/07/2016
+
+import addonHandler
+import globalPluginHandler
+import api
+import config
+import speech
+import wx
+import gui
+from gui import SettingsDialog
+from globalCommands import SCRCAT_CONFIG
+
+addonHandler.initTranslation()
+
+confspec = {
+	"speakTypedSymbols": "boolean(default=False)",
+}
+config.conf.spec["reportSymbols"] = confspec
+
+class AddonSettingsDialog(SettingsDialog):
+
+	# Translators: title of a dialog.
+	title = _("Report Symbols settings")
+
+	def makeSettings(self, settingsSizer):
+		# Translators: label of a dialog.
+		self.reportSymbolsCheckBox = wx.CheckBox(self, wx.NewId(), label=_("&Report symbols"))
+		self.reportSymbolsCheckBox.SetValue(config.conf["reportSymbols"]["speakTypedSymbols"])
+		settingsSizer.Add(self.reportSymbolsCheckBox, border=10, flag=wx.BOTTOM)
+
+	def postInit(self):
+		self.reportSymbolsCheckBox.SetFocus()
+
+	def onOk(self,evt):
+		config.conf["reportSymbols"]["speakTypedSymbols"] = self.reportSymbolsCheckBox.GetValue()
+		super(AddonSettingsDialog, self).onOk(evt)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	def __init__(self):
+		super(globalPluginHandler.GlobalPlugin, self).__init__()
+		self.prefsMenu = gui.mainFrame.sysTrayIcon.preferencesMenu
+		self.settingsItem = self.prefsMenu.Append(wx.ID_ANY,
+			# Translators: name of a menu item.
+			_("Report &Symbols settings..."))
+		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onSettings, self.settingsItem)
+
+	def terminate(self):
+		try:
+			self.prefsMenu.RemoveItem(self.settingsItem)
+		except wx.PyDeadObjectError:
+			pass
+
+	def event_typedCharacter(self, obj, nextHandler, ch):
+		nextHandler()
+		if not config.conf["reportSymbols"]["speakTypedSymbols"] or config.conf["keyboard"]["speakTypedCharacters"] or api.isTypingProtected() or not ["keyboard"]["speechInterruptForCharacters"]:
+			return
+		if not ch.isalnum() and not ch.isspace() and ord(ch)>=32:
+			speech.speakSpelling(ch)
+
+	def onSettings(self, evt):
+		gui.mainFrame._popupSettingsDialog(AddonSettingsDialog)
+
+	def script_settings(self, gesture):
+		wx.CallAfter(self.onSettings, None)
+	script_settings.category = SCRCAT_CONFIG
+	# Translators: message presented in input mode.
+	script_settings.__doc__ = _("Shows the Report Symbols settings dialog.")
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Report Symbols #
+* Author: Noelia Ruiz Martínez.
+
+This add-on allows to listen the typed symbols (non alphanumeric characters), even when the speaking of typed characters is turned off in NVDA.
+
+It's based on the old ReportSymbols add-on,. You should uninstall it to use this version.
+
+## Preferences menu ##
+* Report Symbols settings: Allows to set preferences for the speaking of typed symbols.
+
+Note: A gesture to open this dialog can be assigned from NVDA menu, Preferences submenu, Input gestures dialog, Configuration category.
