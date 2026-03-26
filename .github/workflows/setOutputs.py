6942e0b1334d3ab86351ccfc38aec0bcb3be6f44
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-03-26 15:59:17 +0100

    Simplify workflow

--- .github/workflows/setOutputs.py
+++ .github/workflows/setOutputs.py
@@ -12,46 +12,10 @@ import sha256
 
 def main():
 	addonId = buildVars.addon_info["addon_name"]
-	readmeFile = os.path.join(os.getcwd(), "readme.md")
-	i18nSources = sorted(buildVars.i18nSources)
-	readmeSha = None
-	i18nSourcesSha = None
-	shouldUpdateMd = False
-	shouldUpdatePot = False
-	shouldAddMdFromScratch = False
-	shouldAddPotFromScratch = False
-	if os.path.isfile(readmeFile):
-		readmeSha = sha256.sha256_checksum([readmeFile])
-	i18nSourcesSha = sha256.sha256_checksum(i18nSources)
-	hashFile = os.path.join(os.getcwd(), "hash.json")
-	data = dict()
-	if os.path.isfile(hashFile):
-		with open(hashFile, "rt") as f:
-			data = json.load(f)
-		shouldUpdateMd = data.get("readmeSha") != readmeSha and data.get("readmeSha") is not None
-		shouldUpdatePot = (
-			data.get("i18nSourcesSha") != i18nSourcesSha and data.get("i18nSourcesSha") is not None
-		)
-	shouldAddMdFromScratch = data.get("readmeSha") is None
-	shouldAddPotFromScratch = data.get("i18nSourcesSha") is None
-	if readmeSha is not None:
-		data["readmeSha"] = readmeSha
-	if i18nSourcesSha is not None:
-		data["i18nSourcesSha"] = i18nSourcesSha
-	with open(hashFile, "wt", encoding="utf-8") as f:
-		json.dump(data, f, indent="\t", ensure_ascii=False)
 	name = "addonId"
 	value = addonId
-	name0 = "shouldUpdateMd"
-	value0 = str(shouldUpdateMd).lower()
-	name1 = "shouldUpdatePot"
-	value1 = str(shouldUpdatePot).lower()
-	name2 = "shouldAddMdFromScratch"
-	value2 = str(shouldAddMdFromScratch).lower()
-	name3 = "shouldAddPotFromScratch"
-	value3 = str(shouldAddPotFromScratch).lower()
 	with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-		f.write(f"{name}={value}\n{name0}={value0}\n{name1}={value1}\n{name2}={value2}\n{name3}={value3}\n")
+		f.write(f"{name}={value}\n")
 
 
 if __name__ == "__main__":
